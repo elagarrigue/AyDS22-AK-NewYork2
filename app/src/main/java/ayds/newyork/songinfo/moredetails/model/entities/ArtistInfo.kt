0945df179a20f6174533ad5da4e8e1145e895706
Genaro08 <genaro.08@hotmail.es>
2022-05-02 20:29:05 -0300

    Actualizamos ArtistInfo

--- app/src/main/java/ayds/newyork/songinfo/moredetails/model/entities/ArtistInfo.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/model/entities/ArtistInfo.kt
@@ -1,4 +1,22 @@
 package ayds.newyork.songinfo.moredetails.model.entities
 
-class ArtistInfo {
+interface Artist{
+    val id : String
+    val artistName: String
+    val artistInfo: String
+    var isLocallyStored: Boolean
+}
+
+data class ArtistInfo(
+    override val id : String,
+    override val artistName: String,
+    override val artistInfo: String,
+    override var isLocallyStored: Boolean = false
+) : Artist
+
+object EmptyArtist : Artist {
+    override val id : String = ""
+    override val artistName: String = ""
+    override val artistInfo: String = ""
+    override var isLocallyStored: Boolean = false
 }
