eb6e1be710be115be6645c2279e2ea7a8675fac8
facundoalvarado9 <facundoalvarado9@gmail.com>
2022-05-04 09:59:53 -0300

    Conexión patrón observer homeController -> homeView

--- app/src/main/java/ayds/newyork/songinfo/moredetails/controller/MoreDetailsController.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/controller/MoreDetailsController.kt
@@ -1,4 +1,35 @@
 package ayds.newyork.songinfo.moredetails.controller
 
-class MoreDetailsController {
+import ayds.newyork.songinfo.home.view.HomeUiEvent
+import ayds.newyork.songinfo.moredetails.model.MoreDetailsModel
+import ayds.newyork.songinfo.moredetails.view.MoreDetailsEvent
+import ayds.newyork.songinfo.moredetails.view.MoreDetailsView
+import ayds.observer.Observer
+
+interface MoreDetailsController {
+    fun setMoreDetailsView(moreDetailsView: MoreDetailsView)
+}
+
+internal class MoreDetailsControllerImpl(
+    private val moreDetailsModel: MoreDetailsModel
+    ) : MoreDetailsController{
+
+    private lateinit var moreDetailsView: MoreDetailsView
+
+    override fun setMoreDetailsView(moreDetailsView: MoreDetailsView) {
+        this.moreDetailsView = moreDetailsView
+        moreDetailsView.moreDetailsEventObservable.subscribe(observer)
+    }
+
+    private val observer: Observer<MoreDetailsEvent> =
+        Observer { value ->
+            when (value) {
+                MoreDetailsEvent.GetArtistInfo -> getArtistInfo()
+            }
+        }
+
+    private fun getArtistInfo() {
+        TODO("No implementada todavía")
+    }
+
 }
--- app/src/main/java/ayds/newyork/songinfo/moredetails/view/MoreDetailsView.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/view/MoreDetailsView.kt
@@ -1,4 +1,49 @@
 package ayds.newyork.songinfo.moredetails.view
 
-class MoreDetailsView {
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import ayds.newyork.songinfo.R
+import ayds.newyork.songinfo.home.view.HomeUiEvent
+import ayds.newyork.songinfo.moredetails.model.MoreDetailsModel
+import ayds.observer.Observable
+import ayds.observer.Subject
+
+interface MoreDetailsView {
+    val moreDetailsEventObservable: Observable<MoreDetailsEvent>
+}
+
+class MoreDetailsViewActivity : AppCompatActivity(), MoreDetailsView {
+
+    private val onActionSubject = Subject<MoreDetailsEvent>()
+    override val moreDetailsEventObservable: Observable<MoreDetailsEvent> = onActionSubject
+    private lateinit var moreDetailsModel: MoreDetailsModel
+
+
+    private lateinit var textAbstract: TextView
+    private lateinit var btnUrl: Button
+    private lateinit var nyTimesImg: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_more_details_view)
+
+        initProperties()
+    }
+
+    private fun initProperties() {
+        textAbstract = findViewById(R.id.textPane2)
+        btnUrl = findViewById(R.id.openUrlButton)
+        nyTimesImg = findViewById(R.id.imageView)
+    }
+
+    private fun notifyGetArtistInfoAction() {
+        onActionSubject.notify(MoreDetailsEvent.GetArtistInfo)
+    }
+
+    companion object {
+        const val ARTIST_NAME_EXTRA = "artistName"
+    }
 }
