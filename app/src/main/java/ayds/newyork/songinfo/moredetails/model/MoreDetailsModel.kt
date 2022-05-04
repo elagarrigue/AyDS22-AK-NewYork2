7fe1e77d5e3201047eaa6512f3e7be45add4c76d
Genaro08 <genaro.08@hotmail.es>
2022-05-03 21:04:39 -0300

    Actualizacion MoreDetailsModel

--- app/src/main/java/ayds/newyork/songinfo/moredetails/model/MoreDetailsModel.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/model/MoreDetailsModel.kt
@@ -1,4 +1,31 @@
 package ayds.newyork.songinfo.moredetails.model
 
-class MoreDetailsModel {
+import ayds.newyork.songinfo.home.model.entities.Song
+import ayds.newyork.songinfo.home.model.repository.SongRepository
+import ayds.newyork.songinfo.moredetails.model.entities.Artist
+import ayds.newyork.songinfo.moredetails.model.repository.ArtistInfoRepository
+import ayds.observer.Subject
+
+interface MoreDetailsModel{
+
+    // val ArtistObservable: falta implementar
+
+    fun searchArtist(term: String)
+
+    fun getArtistById(id: String): Artist
+}
+
+internal class  MoreDetailsModelImpl (private val repository: ArtistInfoRepository) : MoreDetailsModel {
+
+    // override val ArtistObservable = Subject<ArtistInfo>()
+
+    override fun searchArtist(id: String) {
+        repository.getArtistById(id).let{
+            // aca va el notify del ArtistObservable
+        }
+    }
+
+    override fun getArtistById(id : String) : Artist{
+        return repository.getArtistById(id)
+    }
 }
