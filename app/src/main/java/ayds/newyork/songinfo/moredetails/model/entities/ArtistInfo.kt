bf8c35906fddfa6d13a50a0a03dceec9600d47f6
Daniel De Mattia <daniel.de.mattia.7@gmail.com>
2022-05-05 19:40:53 -0300

    Implementación NYTimesToArtistResolverImpl

--- app/src/main/java/ayds/newyork/songinfo/moredetails/model/entities/ArtistInfo.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/model/entities/ArtistInfo.kt
@@ -1,21 +1,18 @@
 package ayds.newyork.songinfo.moredetails.model.entities
 
 interface Artist{
-    val id : String
     val artistName: String
     val artistInfo: String
     var isLocallyStored: Boolean
 }
 
 data class ArtistInfo(
-    override val id : String,
     override val artistName: String,
     override val artistInfo: String,
     override var isLocallyStored: Boolean = false
 ) : Artist
 
 object EmptyArtist : Artist {
-    override val id : String = ""
     override val artistName: String = ""
     override val artistInfo: String = ""
     override var isLocallyStored: Boolean = false
--- app/src/main/java/ayds/newyork/songinfo/moredetails/model/repository/external/nytimes/NYTimesToArtistResolverImpl.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/model/repository/external/nytimes/NYTimesToArtistResolverImpl.kt
@@ -1,10 +1,57 @@
 package ayds.newyork.songinfo.moredetails.model.repository.external.nytimes
 
 import ayds.newyork.songinfo.moredetails.model.entities.ArtistInfo
+import com.google.gson.Gson
+import com.google.gson.JsonObject
+import java.lang.StringBuilder
+import java.util.*
+
+private const val RESPONSE = "response"
+private const val DOCS = "docs"
+private const val ABSTRACT  = "abstract"
+private const val WEB_URL = "web_url"
+
 
 class NYTimesToArtistResolverImpl : NYTimesToArtistResolver {
 
     override fun getArtistFromExternalData(serviceData: String?, artistName: String) : ArtistInfo? {
-        return null
+        return try{
+            serviceData?.getFirstItem()?.let { item ->
+                ArtistInfo(
+                    item.getName(),
+                    item.getInfo(artistName)
+                )
+            }
+        } catch(e : Exception){
+            null
+        }
+    }
+
+    private fun JsonObject.getName(): String {
+        return this[ABSTRACT].asString
+    }
+
+    private fun JsonObject.getInfo(artistName: String): String {
+        return renderAbstractAsHtml(this[WEB_URL].asString,artistName)
+    }
+
+    private fun String?.getFirstItem() : JsonObject {
+        val jsonObject = Gson().fromJson(this, JsonObject::class.java)
+        val resp = jsonObject[RESPONSE].asJsonObject
+        val articles = resp[DOCS].asJsonArray
+        return articles[0].asJsonObject
+    }
+
+    private fun renderAbstractAsHtml(abstract: String, artistName: String): String {
+        val stringBuilder = StringBuilder()
+        stringBuilder.append("<html><div width=400>")
+        stringBuilder.append("<font face=\"arial\">")
+        val textWithBold = abstract
+            .replace("'", " ")
+            .replace("\n", "<br>")
+            .replace("(?i)$artistName".toRegex(), "<b>" + artistName.uppercase(Locale.getDefault()) + "</b>")
+        stringBuilder.append(textWithBold)
+        stringBuilder.append("</font></div></html>")
+        return stringBuilder.toString()
     }
 }
