f59521cef52a1b39a1ad7db468631f0643ff08d1
Genaro08 <genaro.08@hotmail.es>
2022-05-06 16:00:30 -0300

    Implementamos el MoreDetailsModelInjector

---
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/model/MoreDetailsModelInjector.kt
@@ -0,0 +1,25 @@
+package ayds.newyork.songinfo.moredetails.model
+
+import android.content.Context
+import ayds.newyork.songinfo.moredetails.model.repository.ArtistInfoRepository
+import ayds.newyork.songinfo.moredetails.model.repository.ArtistInfoRepositoryImpl
+import ayds.newyork.songinfo.moredetails.model.repository.external.nytimes.NYTimesService
+import ayds.newyork.songinfo.moredetails.model.repository.local.nytimes.NYTimesLocalStorage
+import ayds.newyork.songinfo.moredetails.model.repository.local.nytimes.sqldb.NYTimesLocalStorageImpl
+import ayds.newyork.songinfo.moredetails.view.MoreDetailsView
+
+
+object MoreDetailsModelInjector {
+
+    private lateinit var moreDetailsModel: MoreDetailsModel
+
+    fun getHomeModel(): MoreDetailsModel = moreDetailsModel
+
+    fun initHomeModel(moreDetailsView: MoreDetailsView) {
+        val nyTimesLocalStorage : NYTimesLocalStorage = NYTimesLocalStorageImpl(moreDetailsView as Context)
+        val nytService : NYTimesService = NYTimesInjector.spotifyService
+        val repository : ArtistInfoRepository = ArtistInfoRepositoryImpl()
+        moreDetailsModel = MoreDetailsModelImpl(repository)
+    }
+
+}
