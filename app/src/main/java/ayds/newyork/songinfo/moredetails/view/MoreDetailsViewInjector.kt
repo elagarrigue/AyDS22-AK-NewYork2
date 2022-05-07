22ecfff148456730bf3e4ef7adc13c5e0d1627c9
Martin Maslein <tincho_m98@hotmail.com>
2022-05-07 14:26:08 -0300

    Implementados getArtistByName y getArtistById

--- app/src/main/java/ayds/newyork/songinfo/moredetails/view/MoreDetailsViewInjector.kt
+++ app/src/main/java/ayds/newyork/songinfo/moredetails/view/MoreDetailsViewInjector.kt
@@ -1,8 +1,5 @@
 package ayds.newyork.songinfo.moredetails.view
 
-import ayds.newyork.songinfo.home.controller.HomeControllerInjector
-import ayds.newyork.songinfo.home.model.HomeModelInjector
-import ayds.newyork.songinfo.home.view.HomeView
 import ayds.newyork.songinfo.moredetails.controller.MoreDetailsControllerInjector
 import ayds.newyork.songinfo.moredetails.model.MoreDetailsModelInjector
 
