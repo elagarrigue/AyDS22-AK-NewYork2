465a86134bd1dc635a7eebba4f191f96a1bd64e3
Martin Maslein <tincho_m98@hotmail.com>
2022-05-16 20:12:45 -0300

    Avances en los test de la view

--- app/src/androidTest/java/ayds/newyork/songinfo/moredetails/view/ArtistInfoDescriptionHelperImplTest.kt
+++ app/src/androidTest/java/ayds/newyork/songinfo/moredetails/view/ArtistInfoDescriptionHelperImplTest.kt
@@ -1,5 +1,45 @@
 package ayds.newyork.songinfo.moredetails.view
 
-import junit.framework.TestCase
+import ayds.newyork.songinfo.moredetails.model.entities.Artist
+import ayds.newyork.songinfo.moredetails.model.entities.ArtistInfo
+import org.junit.Assert
+import org.junit.Test
 
-class ArtistInfoDescriptionHelperImplTest : TestCase()
+class ArtistInfoDescriptionHelperImplTest {
+
+    private val artistInfoDescriptionHelperImplTest = ArtistInfoDescriptionHelperImpl()
+
+    @Test
+    fun `given a local artist whose article does not contain their name it should return the description`(){
+        val artistInfo: Artist = ArtistInfo(
+        "name",
+        "[*] The Daily Caller is giving away one gun a week until Election Day.",
+        "articleUrl",
+        true
+        )
+
+        val result = artistInfoDescriptionHelperImplTest.getArtistInfoText(artistInfo)
+
+        val expected =
+            "<html><div width=400><font face=\"arial\">[*] The Daily Caller is giving away one gun a week until Election Day.</font></div></html>"
+
+        Assert.assertEquals(expected, result)
+    }
+
+    @Test
+    fun `given a external artist whose article does not contain their name it should return the description`(){
+        val artistInfo: Artist = ArtistInfo(
+            "name",
+            "The Daily Caller is giving away one gun a week until Election Day.",
+            "articleUrl",
+            false
+        )
+
+        val result = artistInfoDescriptionHelperImplTest.getArtistInfoText(artistInfo)
+
+        val expected =
+            "<html><div width=400><font face=\"arial\">[*] The Daily Caller is giving away one gun a week until Election Day.</font></div></html>"
+
+        Assert.assertEquals(expected, result)
+    }
+}
