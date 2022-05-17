e7ec9e84c103895975d670b52f14f0247b7e4a36
Genaro08 <genaro.08@hotmail.es>
2022-05-17 15:51:01 -0300

    implementacion MoreDetailsModelImplTest

--- app/src/test/java/ayds/newyork/songinfo/moredetails/model/MoreDetailsModelImplTest.kt
+++ app/src/test/java/ayds/newyork/songinfo/moredetails/model/MoreDetailsModelImplTest.kt
@@ -1,5 +1,32 @@
 package ayds.newyork.songinfo.moredetails.model
 
-import junit.framework.TestCase
+import ayds.newyork.songinfo.moredetails.model.entities.Artist
+import ayds.newyork.songinfo.moredetails.model.repository.ArtistInfoRepository
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Test
 
-class MoreDetailsModelImplTest : TestCase()
+class MoreDetailsModelImplTest {
+
+    private val repository: ArtistInfoRepository = mockk()
+
+    private val moreDetailsModel: MoreDetailsModel by lazy {
+        MoreDetailsModelImpl(repository)
+    }
+
+    @Test
+    fun `on search song it should notify the result`() {
+        val artist: Artist = mockk()
+        every { repository.getArtistByName("name") } returns artist
+        val artistTester: (Artist) -> Unit = mockk(relaxed = true)
+        moreDetailsModel.artistObservable.subscribe {
+            artistTester(it)
+        }
+
+        moreDetailsModel.searchArtist("name")
+
+        verify { artistTester(artist) }
+    }
+
+}
