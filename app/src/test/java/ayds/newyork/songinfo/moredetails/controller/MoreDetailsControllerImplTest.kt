92c0d8fbc5f58da4fa19cda3c67e493128af3654
Daniel De Mattia <daniel.de.mattia.7@gmail.com>
2022-05-18 12:00:19 -0300

    Implementación MoreDetailsControllerImplTest

--- app/src/test/java/ayds/newyork/songinfo/moredetails/controller/MoreDetailsControllerImplTest.kt
+++ app/src/test/java/ayds/newyork/songinfo/moredetails/controller/MoreDetailsControllerImplTest.kt
@@ -1,5 +1,49 @@
 package ayds.newyork.songinfo.moredetails.controller
 
-import junit.framework.TestCase
+import ayds.newyork.songinfo.moredetails.model.MoreDetailsModel
+import ayds.newyork.songinfo.moredetails.view.MoreDetailsEvent
+import ayds.newyork.songinfo.moredetails.view.MoreDetailsUiState
+import ayds.newyork.songinfo.moredetails.view.MoreDetailsView
+import ayds.observer.Subject
+import io.mockk.every
+import io.mockk.mockk
+import io.mockk.verify
+import org.junit.Before
+import org.junit.Test
 
-class MoreDetailsControllerImplTest : TestCase()
+class MoreDetailsControllerImplTest {
+
+    private val moreDetailsModel: MoreDetailsModel = mockk(relaxUnitFun = true)
+
+    private val onActionSubject = Subject<MoreDetailsEvent>()
+    private val moreDetailsView: MoreDetailsView = mockk(relaxUnitFun = true) {
+        every { moreDetailsEventObservable } returns onActionSubject
+    }
+
+    private val moreDetailsController by lazy {
+        MoreDetailsControllerImpl(moreDetailsModel)
+    }
+
+    @Before
+    fun setup() {
+        moreDetailsController.setMoreDetailsView(moreDetailsView)
+    }
+
+    @Test
+    fun `on search event should search artist`() {
+        every { moreDetailsView.uiState } returns MoreDetailsUiState(name = "name",article = "article",url = "url",false)
+
+        onActionSubject.notify(MoreDetailsEvent.GetArtistInfo)
+
+        verify { moreDetailsModel.searchArtist("name") }
+    }
+
+    @Test
+    fun `on open artist info link event should open external link`() {
+        every { moreDetailsView.uiState } returns MoreDetailsUiState(name = "name",article = "article",url = "url",false)
+
+        onActionSubject.notify(MoreDetailsEvent.OpenArtistInfoLink)
+
+        verify { moreDetailsView.openExternalLink("url") }
+    }
+}
