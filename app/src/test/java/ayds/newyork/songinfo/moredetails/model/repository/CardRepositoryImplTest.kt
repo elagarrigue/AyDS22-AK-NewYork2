4fcc2b493f3d584c31ef48a71c8cba6d63e97255
Genaro08 <genaro.08@hotmail.es>
2022-06-07 23:06:21 -0300

    correcciones CardRepositoryImplTest

--- app/src/test/java/ayds/newyork/songinfo/moredetails/model/repository/CardRepositoryImplTest.kt
+++ app/src/test/java/ayds/newyork/songinfo/moredetails/model/repository/CardRepositoryImplTest.kt
@@ -10,6 +10,7 @@ import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Test
 
 
@@ -19,13 +20,15 @@ class CardRepositoryImplTest {
     private val localStorage: LocalStorage = mockk(relaxUnitFun = true)
     private val brokerService: Broker = mockk(relaxUnitFun = true)
 
+    var list: MutableList<Card> = mutableListOf()
+
     private val cardRepository: CardRepository by lazy {
         CardRepositoryImpl(localStorage, brokerService)
     }
 
-    @Test
-    fun `given existing artist by name should return artist and mark it as local`() {
-        val card1 = ExternalCard(
+    @Before
+    fun init(){
+        var card1 = ExternalCard(
             "name",
             "description",
             "urlInfo",
@@ -33,26 +36,22 @@ class CardRepositoryImplTest {
             "logo",
             false
         )
-        val card2 = ExternalCard(
-            "name",
-            "description",
-            "urlInfo",
-            Source.WIKIPEDIA,
-            "logo",
-            false
-        )
-        val card3 = ExternalCard(
-            "name",
-            "description",
-            "urlInfo",
-            Source.LASTFM,
-            "logo",
-            false
-        )
-        var list: List<Card> = mutableListOf(card1,card2,card3)
+
+        var card2 = card1.copy(source = Source.LASTFM)
+        var card3 = card1.copy(source = Source.WIKIPEDIA)
+
+        list.add(card1)
+        list.add(card2)
+        list.add(card3)
+    }
+
+
+    @Test
+    fun `given existing artist by name should return artist and mark it as local`() {
         every { localStorage.getCardsByName("name") } returns list
 
         val result = cardRepository.getCardsByName("name")
+
         Assert.assertEquals(list, result)
         list.forEach{
             Assert.assertTrue(it.isLocallyStored)
@@ -62,33 +61,9 @@ class CardRepositoryImplTest {
 
     @Test
     fun `given non existing artist by name should get the artist and store it`() {
-        val card1 = ExternalCard(
-            "name",
-            "description",
-            "urlInfo",
-            Source.NEWYORKTIMES,
-            "logo",
-            false
-        )
-        val card2 = ExternalCard(
-            "name",
-            "description",
-            "urlInfo",
-            Source.WIKIPEDIA,
-            "logo",
-            false
-        )
-        val card3 = ExternalCard(
-            "name",
-            "description",
-            "urlInfo",
-            Source.LASTFM,
-            "logo",
-            false
-        )
-        var list: List<Card> = mutableListOf(card1,card2,card3)
         every { localStorage.getCardsByName("name") } returns emptyList()
         every { brokerService.getCards("name") } returns list
+
         val result = cardRepository.getCardsByName("name")
 
         Assert.assertEquals(list, result)
